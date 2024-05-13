7eff1cfcf1895db8415721fb831207d8842470f1
Papi, Soheib <up.sohaib@hotmail.com>
2024-05-13 10:07:39 +0330

    Implementing TraversePreOrderRecursive. Implementing TraversePostOrderRecursive. Implementing TraverseInOrderRecursive.

--- Tree.cs
+++ Tree.cs
@@ -4,18 +4,18 @@ public class Tree
 {
     private class Node
     {
-        public int value { get; set; }
-        public Node leftChild { get; set; }
-        public Node rightChild { get; set; }
+        public int Value { get; set; }
+        public Node LeftChild { get; set; }
+        public Node RightChild { get; set; }
 
         public Node(int value)
         {
-            this.value = value;
+            this.Value = value;
         }
-        
+
         public override String ToString()
         {
-            return "Node=" + value;
+            return "Node=" + Value;
         }
     }
 
@@ -34,23 +34,23 @@ public class Tree
         var current = root;
         while (true)
         {
-            if (value < current.value)
+            if (value < current.Value)
             {
-                if (current.leftChild == null)
+                if (current.LeftChild == null)
                 {
-                    current.leftChild = node;
+                    current.LeftChild = node;
                     break;
                 }
-                current = current.leftChild;
+                current = current.LeftChild;
             }
             else
             {
-                if (current.rightChild == null)
+                if (current.RightChild == null)
                 {
-                    current.rightChild = node;
+                    current.RightChild = node;
                     break;
                 }
-                current = current.rightChild;
+                current = current.RightChild;
             }
         }
     }
@@ -60,13 +60,59 @@ public class Tree
         var current = root;
         while (current != null)
         {
-            if (value < current.value)
-                current = current.leftChild;
-            else if (value > current.value)
-                current = current.rightChild;
+            if (value < current.Value)
+                current = current.LeftChild;
+            else if (value > current.Value)
+                current = current.RightChild;
             else
                 return true;
         }
         return false; //Not found
+    }
+
+    private void TraversePreOrderRecursive(Node root)
+    {
+        if (root == null)
+            return;
+
+        Console.WriteLine(root.Value);
+        TraversePreOrderRecursive(root.LeftChild);
+        TraversePreOrderRecursive(root.RightChild);
+    }
+
+    public void TraversePreOrderRecursive()
+    {
+        TraversePreOrderRecursive(root);
+    }
+
+
+    private void TraversePostOrderRecursive(Node root)
+    {
+        if (root == null)
+            return;
+
+        TraversePostOrderRecursive(root.LeftChild);
+        TraversePostOrderRecursive(root.RightChild);
+        Console.WriteLine(root.Value);
+    }
+
+    public void TraversePostOrderRecursive()
+    {
+        TraversePostOrderRecursive(root);
+    }
+
+    private void TraverseInOrderRecursive(Node root)
+    {
+        if (root == null)
+            return;
+
+        TraverseInOrderRecursive(root.LeftChild);
+        Console.WriteLine(root.Value);
+        TraverseInOrderRecursive(root.RightChild);
+    }
+
+    public void TraverseInOrderRecursive()
+    {
+        TraverseInOrderRecursive(root);
     }
 }
